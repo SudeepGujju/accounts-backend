7ecdb8031bbf3de795f1261b3cbb934fe921bf33
sudeepasmith8@gmail.com <sudeep.gujju@pragmaticplay.com>
2023-04-29 14:47:51 +0530

    error handling

--- utils/format.js
+++ utils/format.js
@@ -5,18 +5,18 @@ function parseCSVDate(date) {
 
     try{
         if (!date) {
-            throw InvalidDateFormatError('Invalid date');
+            throw new InvalidDateFormatError('Invalid date');
         }
     
         let momentDate = moment(date + " +05:30", 'DD-MMM-YYYY ZZ', true);
     
         if (!momentDate.isValid()) {
-            throw InvalidDateFormatError('Invalid date');
+            throw new InvalidDateFormatError('Invalid date');
         }
     
         return momentDate.toDate();
     }catch(err){
-        throw InvalidDateFormatError('Invalid date');
+        throw new InvalidDateFormatError('Invalid date');
     }
 }
 
